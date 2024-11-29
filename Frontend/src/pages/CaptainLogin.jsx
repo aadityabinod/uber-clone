7d44646600d914832cd18c5fd675be55487fea3e
Aaditya Binod Yadav <aadityarayyadav@gmail.com>
2024-11-29 22:54:06 +0545

    captain auth

--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,20 +1,63 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { CaptainDataContext } from '../context/CaptainContext'
+import axios  from 'axios'
 
 
 function CaptainLogin() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {captain, setCaptain} =  useContext(CaptainDataContext)
+    const navigate = useNavigate()
+
+    const submitHandler = async(e)=>{
+      e.preventDefault()
+
+      try {
+
+        const captain = {
+          email: email,
+          password: password
+        }
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captain/login`, captain)
+
+        if (response.status === 200) {
+          const data = response.data
+    
+          setCaptain(data.captain)
+          localStorage.setItem('token', data.token)
+          navigate('/captain-home')
+    
+        }
+    
+        setEmail('')
+        setPassword('')
+        
+      } catch (error) {
+        console.error("login error:", error.response?.data || error.message);
+        
+      }
+    } 
+
   return (
     <div className='p-7 h-screen flex flex-col justify-between'>
       <div>
         <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
 
-        <form>
+        <form onSubmit={(e) => {
+          submitHandler(e)
+        }}>
           <h3 className='text-lg font-medium mb-2'>What's your email</h3>
           <input
             required
             className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
             type="email"
             placeholder='email@example.com'
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
           />
 
           <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
@@ -24,6 +67,10 @@ function CaptainLogin() {
             className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
             required type="password"
             placeholder='password'
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
           />
 
           <button
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,19 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserDataContext } from '../context/UserContext'
+import axios from 'axios'
 
 
 function UserLogin() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [userData, setUserData] = useState({})
+
+
+  const {user, setUser} = useContext(UserDataContext)
+  const navigate = useNavigate()
+
+  const submitHandler = async(e)=>{
+    e.preventDefault()
+    try {
+      const UserData = {
+        email: email,
+        password: password
+      }
+  
+      const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/user/login`, UserData)
+  
+      if(response.status === 200){
+        const data = response.data
+        setUser(data.user)
+        localStorage.setItem('token', data.token)
+        navigate('/home')
+      }
+  
+      setEmail('')
+      setPassword('')
+    } catch (error) {
+      console.error('Login error:', error.response?.data || error.message)
+      // Optional: Add user-friendly error display
+    }
+  }
+
   return (
     <div className='p-7 h-screen flex flex-col justify-between'>
     <div>
       <img className='w-16 mb-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s" alt="" />
 
-      <form >
+      <form onSubmit={(e) => {
+          submitHandler(e)
+        }}>
         <h3 className='text-lg font-medium mb-2'>What's your email</h3>
         <input
           className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
           type="email"
           placeholder='email@example.com'
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
         />
 
         <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
@@ -22,6 +64,10 @@ function UserLogin() {
             className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
             required type="password"
             placeholder='password'
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
           />
 
           <button
